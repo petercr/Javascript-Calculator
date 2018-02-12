83c8f31e30c681d72a7f328423a59b860f0b5afd
Peter <petecruckshank@gmail.com>
2018-02-11 21:52:34 -0500

    added main func with buttonList, -g array to nums And an eventListener to all the buttons in a for loop

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-// alert('woah!!!!');
+const numbersArray = [];
+
+(function () {
+    // get a Node list of all of the buttons
+    const buttonList = document.querySelectorAll('button');
+    console.dir(buttonList);
+
+    for (let i of buttonList) {
+        console.log(i.id);
+        i.addEventListener('click', function () {
+            console.log('button press');
+        })
+    }
+
+})(); // end of self-executing function ()
