1bcc86af2adc5ac86796580cb4a6ec271cccc8be
Peter <petecruckshank@gmail.com>
2018-02-13 21:11:45 -0500

    added the rest of the switch( ) statement for all the easy keys. No equals or history buttons yet

--- src/index.js
+++ src/index.js
@@ -13,18 +13,57 @@ let calcDisplay = '0';
         })
     }
 
-    
+
 })(); // end of self-executing function ()
 
-function changeCalc (keyValue) {
+function changeCalc(keyValue) {
     let currentVal = document.getElementById('visableNums');
     console.log(currentVal.innerText);
-    if (currentVal.innerText == 0){
+    if (currentVal.innerText == 0) {
         currentVal.innerText = '';
     }
-    switch(keyValue){
+    switch (keyValue) {
         case (1):
             currentVal.innerText += 1;
+            break;
+        case (2):
+            currentVal.innerText += 2;
+            break;
+        case (3):
+            currentVal.innerText += 3;
+            break;
+        case (4):
+            currentVal.innerText += 4;
+            break;
+        case (5):
+            currentVal.innerText += 5;
+            break;
+        case (6):
+            currentVal.innerText += 6;
+            break;
+        case (7):
+            currentVal.innerText += 7;
+            break;
+        case (8):
+            currentVal.innerText += 8;
+            break;
+        case (9):
+            currentVal.innerText += 9;
+            break;
+        case (0):
+            currentVal.innerText += 0;
+            break;
+        case ('+'):
+            currentVal.innerText += ' + \ \n';
+            break;
+        case ('*'):
+            currentVal.innerText += ' * \ \n';
+            break;
+        case ('/'):
+            currentVal.innerText += ' / \ \n';
+            break;
+        case ('-'):
+            currentVal.innerText += ' - \ \n';
             break;
     }
 }
