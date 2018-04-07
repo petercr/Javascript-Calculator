d8c8dd167af351f898778656b47b49661a019723
Peter Cruckshank <petecruckshank@gmail.com>
2018-04-07 10:34:05 -0400

    added new ESLint config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,29 @@
 module.exports = {
-    "extends": "airbnb-base"
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
 };
